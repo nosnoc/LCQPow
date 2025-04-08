9ec4ef05cd3d4aba960a00e496c6a54ec4f5e689
Anton Edvinovich Pozharskiy <apozharski@gmail.com>
2025-04-08 16:49:42 +0200

    Begin on new interface with object storage

--- interfaces/matlab/@LCQProblem/LCQProblem.m
+++ interfaces/matlab/@LCQProblem/LCQProblem.m
@@ -1,16 +1,29 @@
 classdef LCQProblem < handle
     properties(Access=private)
-        self % ptr to C++ LCQProblem object
+        self % ptr to C++ LCQProblem object. On a 64 bit x86
     end
 
     methods
         % Constuctor
-        obj = LCQProblem(nV, nC, nComp);
+        function obj = LCQProblem(nV, nC, nComp)
+            obj.constructProblem(nV, nC, nComp);
+        end
+
 
         %
         loadLCQP(obj, args);
 
         %
         runSolver();
+
+        function delete(obj)
+        % obj is always scalar, if it isn't we panic :)
+            obj.destructProblem();
+        end
+    end
+
+    methods(Access=private)
+        constructProblem(obj, nV, nC, nComp);
+        destructProblem(obj);
     end
 end
